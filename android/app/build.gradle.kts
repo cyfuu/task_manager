9c92ffe3b7bf794d47d0c05cc2bf545bc87200b5
cyfuu_ <brianjosepharatia@gmail.com>
2026-04-17 20:14:47 +0800

    fix: improve properties loading and syntax in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,15 @@
-def keystoreProperties = new Properties()
-def keystorePropertiesFile = rootProject.file('key.properties')
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(
-        new FileInputStream(keystorePropertiesFile))
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -23,14 +24,13 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "ph.edu.usjr.first_app.task_manager"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -38,19 +38,19 @@ android {
     }
 
     signingConfigs {
-        release {
-            keyAlias = keystoreProperties["keyAlias"] ?: System.getenv("KEY_ALIAS")
-            keyPassword = keystoreProperties["keyPassword"] ?: System.getenv("KEY_PASSWORD")
-            storeFile = keystoreProperties["storeFile"]?.let { 
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias") ?: System.getenv("KEY_ALIAS")
+            keyPassword = keystoreProperties.getProperty("keyPassword") ?: System.getenv("KEY_PASSWORD")
+            storeFile = keystoreProperties.getProperty("storeFile")?.let { 
                 file(it) 
-            } ?: file(System.getenv("KEYSTORE_PATH"))
-            storePassword = keystoreProperties["storePassword"] ?: System.getenv("STORE_PASSWORD")
+            } ?: file(System.getenv("KEYSTORE_PATH") ?: "")
+            storePassword = keystoreProperties.getProperty("storePassword") ?: System.getenv("STORE_PASSWORD")
         }
     }
     
     buildTypes {
         release {
-            signingConfig signingConfigs.release
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
